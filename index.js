198c1c60c4223ac2ac2765823f0bd930400c2852
shrinidhi <shrinidhi.uppoor@jalantechnologies.com>
2022-02-02 10:48:36 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const number = parseInt(prompt('Enter the number of terms: '));
 let n1 = 0, n2 = 1, nextTerm;
 
-console.log('Fibonacci Series:');
+console.log('Fibonacci Series:  '  );
 
 for (let i = 1; i <= number; i++) {
-    console.log(n1);
+    console.log(n1, "");
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
+
 }
